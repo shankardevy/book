0c7e51df149e20df95320c23a92b1dd25a50606f
Shankar Dhanasekaran <shankardevy@gmail.com>
2020-08-27 15:43:50 +0530

    Initial commit

---
+++ lib/book.ex
@@ -0,0 +1,18 @@
+defmodule Book do
+  @moduledoc """
+  Documentation for `Book`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Book.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
